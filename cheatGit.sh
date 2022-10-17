a01568b61e449be11388ec5e471b27660bfb2441
Lisa Knox <lisaknox83@gmail.com>
2022-10-17 10:02:07 -0400

    Mon 17 Oct 2022 10:02:07 AM EDT (x 3)

--- cheatGit.sh
+++ cheatGit.sh
@@ -2,7 +2,9 @@
 
 cd ~/scripts/cheatGit
 
+ARRAY_OF_ITERATION_VALUES=(0 0 0 3 5 6 9 11 12)
 NUM_OF_ITERATIONS=$(($RANDOM % 4))
+echo ${ARRAY_OF_ITERATION_VALUES[5]}
 
 function iterate {
   git pull
